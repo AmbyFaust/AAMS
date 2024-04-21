12bff2a86a86da9f37de74083b2c5e9152f79f43
Kolyanuch <andreenko_kolya@mail.ru>
2024-04-21 16:51:33 +0300

    simplifying secondary

--- project/Test/Test.py
+++ project/Test/Test.py
@@ -1,31 +1,40 @@
 import math
 import numpy as np
 from project.modeling.ObjectModels.RadarObj import RadarObj,mark,radar_params,signal_params,RectCS
+from project.modeling.ObjectModels.CommandPostObj import CommandPostObj
 from project.modeling.CSTransformator import GRCStoUV
 from project.modeling.CSTransformator import UVtoGRCS
 
-start_coords = RectCS(0, 0, 0)
 start_time = 0
-RadarParams1 = radar_params(1000, 2, BW_U=3, BW_V=3, Scanning_V=[10,70])
+RadarParams1 = radar_params(1000, 2, BW_U=3, BW_V=3)
 PRF = 10 ** 5
 SignalTime = 10 ** (-6)
 NPulsesProc = 1000
 OperatingFreq = 15 * 10 ** 9
 Signal1 = signal_params(PRF, SignalTime, NPulsesProc, OperatingFreq)
-Svarog = RadarObj(start_coords, RadarParams1, Signal1, start_time)
+# свароги
+Svarog_1 = RadarObj(RectCS(0, 0, 0), RadarParams1, Signal1, start_time,1)
+Svarog_2 = RadarObj(RectCS(-200, 200, 10), RadarParams1, Signal1, start_time,2)
+Svarog_3 = RadarObj(RectCS(20, 300, 5), RadarParams1, Signal1, start_time,3)
 
+all_radars = [Svarog_1 , Svarog_2, Svarog_3]
+Comman_Post_SKB_7 = CommandPostObj
 
+# print(Svarog_2.StartCoords)
+# print(Svarog_3.StartCoords)
 
-x0 = 50
-y0 = 50
-z0 = 50
-Velocity = 300
+x0 = 3000
+y0 = 5000
+z0 = 6000
+Velocity = -300
 phi_U = 45 #azimut
 phi_V = 45 #elevation
+target_id = 56
 
-periods_of_scanning = 10
+periods_of_scanning = 5
 t_btw_scanning = 0.1
-for one_scan in range (1,periods_of_scanning+1):
+
+for one_scan in range(1, periods_of_scanning+1):
     t = t_btw_scanning*(one_scan-1)
     x = x0 + Velocity*np.cos(math.radians(phi_V))*t
     y = y0 + Velocity*np.cos(math.radians(phi_V))*t
@@ -35,8 +44,21 @@ for one_scan in range (1,periods_of_scanning+1):
     stdU = 0.07
     stdV = 0.07
     stdR = 20
-    marochka = mark(u,v,r,stdU,stdV,stdR)
-    Svarog.secondary_processing(marochka)
-
-trajes = Svarog.Trajectories[0]
-print(trajes.marks)
+    marochka = mark(u,v,r,stdU,stdV,stdR,target_id)
+    Svarog_1.secondary_processing(marochka)
+    Svarog_2.secondary_processing(marochka)
+    Svarog_3.secondary_processing(marochka)
+    # for one_radar in range(0,1):
+    #     radar = all_radars[one_radar]
+    #     all_radar_traj = radar.Trajectories
+    #     for one_traj in range(0, len(all_radar_traj)):
+    #         current_traj = all_radar_traj[one_traj]
+    #         print(current_traj.is_confimed)
+    #         print('saad')
+            # if(current_traj.is_confimed == True):
+            #     print('ebash')
+# trajes = Svarog_1.Trajectories[0]
+# print(Svarog_1.Trajectories,Svarog_2.Trajectories)
+# mark1 = mark(3,3,3,3,3,3,3)
+# mark1 = mark1._replace(V=5)
+# print(mark1)
--- project/modeling/ObjectModels/RadarObj.py
+++ project/modeling/ObjectModels/RadarObj.py
@@ -7,26 +7,22 @@ from project.modeling.CSTransformator import GRCStoUV
 from project.modeling.CSTransformator import UVtoGRCS
 
 # объявляем используемые типы данных
-radar_params = namedtuple('radar_params', 'EIRP Seff  BW_U BW_V Scanning_V')
+radar_params = namedtuple('radar_params', 'EIRP Seff  BW_U BW_V')
 signal_params = namedtuple('signal_params', 'PRF SignalTime NPulsesProc OperatingFreq')
-mark = namedtuple('mark', 'U V R stdU stdV stdR')
+mark = namedtuple('mark', 'U V R stdU stdV stdR target_id')
+trajectory = namedtuple('trajectory', 'stack_of_coords target_id is_confimed')
 target_params = namedtuple('target_params', 'RCS coordinates')
 RectCS = namedtuple('RectCS', 'X Y Z')
 UVCS = ('UVCS', 'U V R')
 
-# Класс Ветка
-class brunch(Object):
-    marks = np.array([])
-    next_gate = []
-    initiated = True
 
 # Класс Радар
 class RadarObj(Object):
-    Trajectories = []
-
 
     # Конструктор Класса
-    def __init__(self, start_coords, radar_params, signal_params, start_time):
+    def __init__(self, start_coords, radar_params, signal_params, start_time, radar_id):
+        self.Trajectories = []
+        self.RadarID = radar_id
         self.StartCoords = start_coords
         self.RadarParams = radar_params
         self.SignalParams = signal_params
@@ -36,14 +32,11 @@ class RadarObj(Object):
         self.wavelength = 3 * 10 ** 8 / signal_params.OperatingFreq
         self.RangeResolution = 3 * 10 ** 8 * signal_params.SignalTime / 2
         self.t_btw_transmiting = signal_params.NPulsesProc/signal_params.PRF
-
-
         # Функция изменения углов сканирования
 
     def ChangeAnglOfViev(self, UBeam, VBeam):
         self.UBeam = UBeam
         self.VBeam = VBeam
-
         # Функция для расчёта ОСШ
 
     def CalculateSNR(self, TargetRange, TargetRCS):
@@ -70,94 +63,52 @@ class RadarObj(Object):
                                            Z=targets_coordsGRCS[i].Z - self.StartCoords.Z)
         return targets_coordsLRCS
 
-    def scanning_procces(self,t):
-        # t кратен времени между зондированиями
-        # t характеризует номер зондирования конкретного радара, при t = 0 положение луча на
-        [Vmin,Vmax] = self.RadarParams.Scanning_V
-        N = t/self.t_btw_transmiting
-        N_V = (Vmax - Vmin)/self.RadarParams.BW_V
-        N_U = 360 / self.RadarParams.BW_U
-        N_of_one_per = N_V * N_U
-        N_in_scan = N - (N // N_of_one_per) * N_of_one_per
-        N_V_in_scan = N_in_scan % (N_V)
-        N_U_in_scan = N_in_scan // (N_V)
-        Current_V = Vmin + (self.RadarParams.BW_V/2)*(1 + N_V_in_scan)
-        Current_U = (self.RadarParams.BW_U/2)*(1 + N_U_in_scan)
-        return [Current_U, Current_V]
-
 
 
     def secondary_processing(self, mark):
+
         u = mark[0]
         v = mark[1]
         r = mark[2]
-        stdU = mark[3]
-        stdV = mark[4]
-        stdR = mark[5]
-        [x,y,z] = UVtoGRCS([r,u,v])
-
-        trafectories = self.Trajectories  # get all trajes
-
-        if not trafectories:
-            self.intitiate_traj(x,y,z,r,u,v)
+        targ_id = mark[6]
+        # trafectories = self.Trajectories  # get all trajes
+        if not self.Trajectories:
+            self.intitiate_traj(targ_id, r, u, v)
         else:
-            for one_traj in range(1, len(trafectories) + 1):
-                current_traj = trafectories[one_traj - 1]
-                if (current_traj.initiated == True):
-                    gates = current_traj.next_gate
-                    if (x > gates[0] and x < gates[1] and y > gates[2] and y < gates[3] and z > gates[4] and z < gates[5]):
-                        curr_traj_marks = current_traj.marks
-                        curr_coord = np.array([[r], [u], [v]])
-                        current_traj.marks = np.concatenate((curr_traj_marks, curr_coord), axis=1)
-                        current_traj.next_gate =  self.get_tracking_gate(current_traj.marks, stdU, stdV, stdR)
-                        current_traj.initiated = False
-                else:
-                    gates = current_traj.next_gate
-                    if (r > gates[0] and r < gates[1] and u > gates[2] and u < gates[3] and v > gates[4] and v < gates[5]):
-                        curr_traj_marks = current_traj.marks
-                        curr_coord = np.array([[r], [u], [v]])
-                        current_traj.marks = np.concatenate((curr_traj_marks, curr_coord), axis=1)
-                        current_traj.next_gate =  self.get_tracking_gate(current_traj.marks, stdU, stdV, stdR)
+            key = 0
+            for one_traj in range(1, len(self.Trajectories) + 1):
+                current_traj = self.Trajectories[one_traj - 1]
+                # print(current_traj.is_confimed)
 
-    def intitiate_traj (self,x, y, z, r, u, v):
-        creating_brunch = brunch
-        creating_brunch.marks = np.array([[r], [u], [v]])
-        creating_brunch.next_gate = self.get_initation_gate(x, y, z)
-        creating_brunch.initiated = True
-        RadarObj.Trajectories.append(creating_brunch)
+                if (current_traj.target_id == targ_id):
+                    key = 1
+                    curr_traj_marks = current_traj.stack_of_coords
+                    curr_coord = np.array([[r], [u], [v]])
+                    # print(self.Trajectories[one_traj - 1].stack_of_coords)
+                    self.Trajectories[one_traj - 1] = current_traj._replace(stack_of_coords = np.concatenate((curr_traj_marks, curr_coord), axis=1))
 
-    def get_initation_gate(self, x, y, z):
-        initation_radius = self.t_btw_transmiting * 500
-        return [x - initation_radius, x + initation_radius, y - initation_radius, y + initation_radius,
-                z - initation_radius, z + initation_radius]
-
-    def get_tracking_gate(self, marks, stdU, stdV, stdR):
-        prev_mark = marks[0:3, -2]
-        last_mark = marks[0:3, -1]
-        last_mark = UVtoGRCS(last_mark)
-        prev_mark = UVtoGRCS(prev_mark)
-        V_x = last_mark[0] - prev_mark[0]
-        V_y = last_mark[1] - prev_mark[1]
-        V_z = last_mark[2] - prev_mark[2]
-        extr_x = last_mark[0] + V_x
-        extr_y = last_mark[1] + V_y
-        extr_z = last_mark[2] + V_z
-        [r_extr, u_extr, v_extr] = GRCStoUV([extr_x, extr_y, extr_z])
-        r1 = r_extr - 3 * stdR
-        r2 = r_extr + 3 * stdR
-        u1 = u_extr - 3 * stdU
-        u2 = u_extr + 3 * stdU
-        v1 = v_extr - 3 * stdV
-        v2 = v_extr + 3 * stdV
-        return [r1, r2, u1, u2, v1, v2]
+                    if (current_traj.is_confimed == False):
+                        self.check_if_its_supostat(one_traj)
+            if key == 0:
+                self.intitiate_traj(targ_id, r, u, v)
+    def intitiate_traj (self,target_id, r, u, v):
+        initiation_traj = trajectory(np.array([[r], [u], [v]]),target_id,False)
+        self.Trajectories.append(initiation_traj)
+    def check_if_its_supostat (self,one_traj):
+        current_traj = self.Trajectories[one_traj-1]
+        [_, l] = current_traj.stack_of_coords.shape
+        if (l == 5):
+            self.Trajectories[one_traj - 1] = current_traj._replace(is_confimed = True)
+            print ('Supostat with id',current_traj.target_id,' is detected by locator with id',self.RadarID)
 
 
 if __name__ == "__main__":
     start_coords = RectCS(0, 0, 0)
     start_time = 0
-    RadarParams1 = radar_params(1000, 2, BW_U=3, BW_V=3, Scanning_V=[10,70])
+    RadarParams1 = radar_params(1000, 2, BW_U=3, BW_V=3)
     Signal1 = signal_params(10 ** 3, 10 ** (-6), 1000, 15 * 10 ** 9)
-    Radar1 = RadarObj(start_coords, RadarParams1, Signal1, start_time)
+    radar_id = 100
+    Radar1 = RadarObj(start_coords, RadarParams1, Signal1, start_time, radar_id)
     # Radar1 = RadarObj()
     Radar1.ChangeAnglOfViev(1, 1)
     targetcoords1 = [RectCS(10, 10, 5)]
